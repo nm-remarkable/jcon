e18ffb40123da9bb6c1cd08a6aa7ee0cce900b58
Nuno Mendes <98030270+nm-remarkable@users.noreply.github.com>
2024-10-26 15:07:28 +0200

    change repo name

--- build.zig
+++ build.zig
@@ -13,7 +13,7 @@ pub fn build(b: *std.Build) void {
     };
 
     const exe = b.addExecutable(.{
-        .name = "jzon",
+        .name = "jcon",
         .target = target,
         .optimize = optimize,
     });
